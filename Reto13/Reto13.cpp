4b87728803568f0d4cd0b79457d4e8444958b7a2
Saul-renedo <jorgesaulrenedo.9201@gmail.com>
2020-05-25 00:45:54 +0000

    Reto 13

--- Reto13/Reto13.cpp
+++ Reto13/Reto13.cpp
@@ -5,7 +5,6 @@
 //El disco J es dibujado con centro en (J, 0) y radio A[J]
 //Un disco J y otro K intersectan si J ≠ K y si al menos existe un punto de contacto entre ellos
 
-
 //Caso particular:
 //Tenemos 6 discos
 //Están numerados de 0 a 5
@@ -13,10 +12,8 @@
 //El disco 3 es dibujado con centro en (3, 0) y radio 1
 //El disco 3 y el disco 4 intersectan si J ≠ K y si al menos existe un punto de contacto entre ellos
 
-
 //Pseudocódigo
 //A[0] = 1;  A[1] = 5;  A[2] = 2;  A[3] = 1;  A[4] = 4;  A[5] = 0;
-
 
     //0. Comparar el disco 0 con todos los siguientes
         // Discoizq = A la posición del disco izq. sumarle el radio
